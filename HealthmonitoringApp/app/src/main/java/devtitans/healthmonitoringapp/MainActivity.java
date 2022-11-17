887593c50399bf0a54faf0659cdd79d77cd19e86
[gabriel.clavila] <[gabriel.clavila@gmail.com]>
2022-11-16 20:05:08 -0400

    Atualizacao da Interface do Aplicativo

--- HealthmonitoringApp/app/src/main/java/devtitans/healthmonitoringapp/MainActivity.java
+++ HealthmonitoringApp/app/src/main/java/devtitans/healthmonitoringapp/MainActivity.java
@@ -9,26 +9,82 @@ import android.widget.*;
 
 import android.os.RemoteException;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 import devtitans.healthmonitoringmanager.HealthmonitoringManager;                          // Biblioteca do Manager
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "DevTITANS.SmartlampApp";
+    private static final String TAG = "DevTITANS.HealthMonitoring";
 
     private TextView textStatus, textResistance;
     private HealthmonitoringManager manager;                                        // Atributo para o Manager
+    private int lastX = 0;
+    private LineGraphSeries<DataPoint> series;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textStatus =     findViewById(R.id.textStatus);
+        //textStatus =     findViewById(R.id.textStatus);
         textResistance =     findViewById(R.id.textResistance);                      // Acessa os componentes da tela
-
 
         manager = HealthmonitoringManager.getInstance();
 
-        updateAll(null);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(2200);
+        viewport.setScrollable(true);
+
+
+        //updateAll(null);
+        start(null);
     }
+
+    public void start(View view){
+        Timer timer = new Timer(false);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("TESTE","Serviço Rodando");
+                int resistance = 0;
+                try {
+                    resistance = manager.getResistance();
+                    Log.d("RESISTENCIA","Serviço Rodando"+resistance);
+                    updateResistance(resistance);
+                } catch (android.os.RemoteException e) {
+                    Log.e(TAG, "Erro atualizando dados:", e);
+                }
+
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask,0000,1000);
+    }
+
+    public void updateResistance(int resist) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                textResistance.setText(String.valueOf(resist));
+                addEntry(resist);
+            }
+        };
+        runOnUiThread(runnable);
+    }
+
+    public void addEntry(int resist){
+        series.appendData(new DataPoint(lastX++, resist),false,5);
+    }
+/*
 
     public void updateAll(View view) {
         Log.d(TAG, "Atualizando dados do dispositivo ...");
@@ -59,5 +115,5 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "Erro atualizando dados:", e);
         }
 
-    }
+    }*/
 }
